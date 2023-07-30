b8791bf4dfb1873d64e343c56622a556bd43fd4b
Pranjal Jain <jainpranjal30112002@gmail.com>
2023-07-30 17:20:56 +0530

    room setup

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useSocket } from '../context/SocketProvider';
 
 const LobbyScreen = () => {
@@ -12,6 +13,18 @@ const LobbyScreen = () => {
         socket.emit("room:join", {email,room})
         
     }, [email,room,socket]);
+
+    const handleJoinRoom = useCallback((data) => {
+        const { email, room } = data;
+        console.log(email, room);
+    },[]);
+
+    useEffect(() => {
+        socket.on("room:join", handleJoinRoom);
+        return () => {
+            socket.off('room:join', handleJoinRoom)
+        }
+    }, [socket,handleJoinRoom]);
 
     return (
         <div>
---
+++ client/src/screens/Room.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RoomPage = () => {
+    return (
+        <div>
+            <h1>
+                Room Page
+            </h1>
+        </div>
+    )
+}
+
+export default RoomPage;
