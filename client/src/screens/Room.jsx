0d971af1be94f1d13e3a0ebf5ffe82b89e532900
Pranjal Jain <jainpranjal30112002@gmail.com>
2023-08-05 23:17:45 +0530

    Video Cal Facility Added.

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,20 +1,28 @@
 import React, { useEffect, useCallback, useState } from 'react';
+import ReactPlayer from 'react-player';
 import { useSocket } from '../context/SocketProvider';
 
 const RoomPage = () => {
     const socket = useSocket();
     const [remoteSocketId, setrRemoteSocketId] = useState(null);
+    const [myStream, setMyStream] = useState();
 
-    const handleUserJoined = useCallback(({email, id}) => {
+    const handleUserJoined = useCallback(({ email, id }) => {
         console.log(`Email ${email} joined the room`);
         setrRemoteSocketId(id)
-    },[]);
+    }, []);
+
+    const handleCallUser = useCallback(async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+
+        setMyStream(stream);
+    }, []);
 
     useEffect(() => {
-        socket.on("user:joined",handleUserJoined);
+        socket.on("user:joined", handleUserJoined);
 
         return () => {
-            socket.off('user:joined',handleUserJoined)
+            socket.off('user:joined', handleUserJoined)
         }
     }, [socket, handleUserJoined]);
     return (
@@ -23,7 +31,15 @@ const RoomPage = () => {
                 Room Page
             </h1>
             <h4>{remoteSocketId ? 'Connected' : 'No one in room'}</h4>
+            {remoteSocketId && <button onClick={handleCallUser}>Call</button>}
+            {myStream &&
+                <>
+                    <h1>My Stream</h1>
+                    <ReactPlayer playing muted height="100px" width="200px" url={myStream} />
+                </>
+            }
         </div>
+
     )
 }
 
